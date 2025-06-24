d59a5cb23bc7df8c34217b87c032adfaf4499579
Brian Zhao <brianzhao@Brians-Air.attlocal.net>
2025-06-24 11:33:55 -0700

    update TACO_GRPO
    Signed-off-by: Brian Zhao <brianzhao@Brians-Air.attlocal.net>

---
+++ scripts/TACO_GRPO/reward_function.py
@@ -0,0 +1,84 @@
+from unittest.mock import patch
+import io
+from contextlib import redirect_stdout
+import multiprocessing
+import time
+import builtins
+import io
+import sys
+import re
+import os
+import platform
+import faulthandler
+import resource
+import builtins
+import json
+
+def compute_score(data_source, solution_str, ground_truth, extra_info=None):
+  print("data_source: ", data_source)
+  print("solution_str: ", solution_str)
+  print("ground_truth: ", ground_truth)
+  loaded_ground = json.loads(ground_truth)
+  case_input = loaded_ground["inputs"]
+  case_output = loaded_ground["outputs"]
+  return test_code(solution_str,case_input,case_output, 100)
+  exit(0)
+
+def run_code(code, case_input, output_queue, memory_limit=256):
+    start = time.time()
+    code = code.replace("python", "")
+    code = code.replace("```", "")
+    if isinstance(case_input, list):
+        case_input = ' '.join(str(s) for s in case_input)
+
+    # Mock input
+    input_lines = iter(case_input.splitlines())
+    builtins.input = lambda: next(input_lines)
+
+    # Capture output
+    buf = io.StringIO()
+    sys.stdout = buf
+    try:
+        exec(code)
+    except Exception as e:
+        output_queue.put(("RUNTIME ERROR\n", 0))
+        return
+    finally:
+        sys.stdout = sys.__stdout__
+
+    output_queue.put(({
+        "output": buf.getvalue(),
+        "time": time.time() - start,
+        "memory": resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
+    }, 0))  # memory will be added later
+
+def test_code(code, cases, ex_out, time_limit):
+    score = 0
+    correct_cases = 0
+
+    for i in range(len(cases)):
+        output_queue = multiprocessing.Queue()
+        p = multiprocessing.Process(target=run_code, args=(code, cases[i], output_queue))
+        p.start()
+        p.join(timeout=time_limit)
+        if p.is_alive():
+            p.terminate()
+            out = "TIME LIMIT EXCEEDED\n"
+            score -= 1 / len(cases) * 10
+        else:
+            try:
+                out, _ = output_queue.get(timeout=1)
+            except:
+                out = "ERROR\n"
+
+            if (out == "ERROR\n" or out == "RUNTIME ERROR\n"):
+                score -= 1 / len(cases) * 50
+
+        if isinstance(out, dict) and out['output'].rstrip() == ex_out[i].rstrip():
+            correct_cases += 1
+        else:
+            print(out)
+    score += correct_cases / len(cases) * 100
+    if correct_cases == len(cases):
+        score += 25
+    return score
---
+++ scripts/TACO_GRPO/run.sh
@@ -0,0 +1,52 @@
+set -x
+
+# If you are using vllm<=0.6.3, you might need to set the following environment variable to avoid bugs:
+# export VLLM_ATTENTION_BACKEND=XFORMERS
+
+export WANDB_API_KEY=dcfd563a07eb5783ee9491008d9da685f043ec2b
+
+HYDRA_FULL_ERROR=1 python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=$HOME/data/gsm8k/TACO_train.parquet \
+    data.val_files=$HOME/data/gsm8k/TACO_test.parquet \
+    data.train_batch_size=4096 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=2048 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    data.shuffle=False \
+    actor_rollout_ref.model.path=Qwen/Qwen2-0.5B-Instruct \
+    actor_rollout_ref.model.lora_rank=64 \
+    actor_rollout_ref.model.lora_alpha=32 \
+    actor_rollout_ref.actor.optim.lr=3e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=2048 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=64 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=64 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.rollout.load_format=safetensors \
+    actor_rollout_ref.rollout.layered_summon=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=64 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.use_kl_in_reward=False \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='verl_grpo_example_gsm8k' \
+    trainer.experiment_name='qwen2.5_3b_grpo_lora' \
+    trainer.n_gpus_per_node=1 \
+    trainer.nnodes=1 \
+    trainer.save_freq=20 \
+    trainer.test_freq=5 \
+    reward_model.enable=False \
+    custom_reward_function.path=reward_function.py \
+    trainer.total_epochs=15
