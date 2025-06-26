421095a61effb1ff829555019a8645d152d178ae
Brian Zhao <brianzhao@Brians-Air.attlocal.net>
2025-06-26 09:48:07 -0700

    fixed minor bugs in load_dataset.py
    Signed-off-by: Brian Zhao <brianzhao@Brians-Air.attlocal.net>

--- scripts/Reasoner/load_dataset.py
+++ scripts/Reasoner/load_dataset.py
@@ -4,166 +4,81 @@ import os
 from typing import cast
 import json
 import datasets
-
+import pandas as pd
 from datasets.load import Dataset, DatasetDict
 from verl.utils.hdfs_io import copy, makedirs
-# added this header
-
-
-def extract_solution(solution_str):
-    solution = re.search("#### (\\-?[0-9\\.\\,]+)", solution_str)
-    assert solution is not None
-    final_solution = solution.group(0)
-    final_solution = final_solution.split("#### ")[1].replace(",", "")
-    return final_solution
-
-
+from tqdm import tqdm
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--local_dir", default="~/data/gsm8k")
-    parser.add_argument("--hdfs_dir", default=None)
-
-    args = parser.parse_args()
-
     data_source = "BAAI/TACO"
-
     dataset: DatasetDict = cast(DatasetDict, datasets.load_dataset(data_source, "ALL"))
-
     train_dataset: Dataset = dataset["train"]
     test_dataset: Dataset = dataset["test"]
+    def make_parquet(split):
+        def make_format(question: str, inputs: list[str], outputs: list[str]):
+            prompts = []
+            ground_truth = []
+            for input, output in zip(inputs, outputs):
+                if (isinstance(input, str) and isinstance(output, str)):
+                    query = question + "\n" + input
+                    answer = output
+                    prompts.append(query)
+                    ground_truth.append(answer)
+            return prompts, ground_truth
 
-    def make_map_fn(split):
-        def process_fn(example, idx):
-            question_raw = example.pop("question")
-
-            question = question_raw
-
-            test_cases = example.pop("input_output")
-            try:
-                test_cases = json.loads(test_cases)
+        prompts = []
+        ground_truth = []
+        print("formatting prompts + groundtruth...")
+        for sample in tqdm(dataset[split]):
+            try: 
+                question = sample['question']
+                inputs = json.loads(sample['input_output'])['inputs']
+                outputs = json.loads(sample['input_output'])['outputs']
+                a, b = make_format(question, inputs, outputs)
+                prompts.extend(a)
+                ground_truth.extend(b)
             except:
-                data = {
-                    "data_source": data_source,
-                    "prompt": [
-                        {
-                            "role": "user",
-                            "content": question,
-                        }
-                    ],
-                    "ability": "math",
-                    "reward_model": {
-                        "style": "rule",
-                        "ground_truth": {"inputs": [], "outputs": []},
-                    },
-                    "extra_info": {
-                        "split": split,
-                        "index": idx,
-                        "question": question_raw,
-                    },
+                continue
+        print("done")
+        assert len(prompts) == len(ground_truth)
+        data_source = ["BAAI/TACO"] * len(prompts)
+        def format_prompt(prompt : str) -> list[dict]:
+            return [
+                {
+                    "role": "system",
+                    "content": """
+    /think
+    You will be given a competitive programming problem with small input test cases. 
+    You need to find the correct output for the given input by reasoning step by step through the test cases without writing any code.
+    """ 
+                },
+                {
+                    "role": "user",
+                    "content": prompt
                 }
-                return data
+            ]
+        print("formatting...")
+        prompt = list(map(format_prompt, tqdm(prompts)))
+        print("done")
+        ability = ["code"] * len(prompts)
+        def format_reward_model(input_output_pair : tuple[str, str]) -> dict:
+            assert isinstance(input_output_pair[0], str)
+            assert isinstance(input_output_pair[1], str)
 
-            inputs: list[str] = test_cases["inputs"]
-            outputs: list[str] = test_cases["inputs"]
-
-
-            def isValid(test_in: str, test_out: str):
-                test_in = test_in.strip()
-                test_out = test_out.strip()
-
-                if len(test_in) > 20 or len(test_out) > 10:
-                    return False
-
-                for num in test_in.split():
-                    if not num.isdigit():
-                        continue
-                    if int(num) > 25:
-                        return False
-
-                for num in test_out.split():
-                    if not num.isdigit():
-                        continue
-                    if int(num) > 25:
-                        return False
-                return True
-
-            def remove_long(data) -> bool:
-                if isinstance(data[0], str) and isinstance(data[1], str):
-                    return isValid(data[0], data[1])
-                return False
-
-            sys_prompt = f"""
-You will be given a competitive programming problem with small input test cases. 
-You need to find the correct output for the given input by reasoning step by step through the test cases without writing any code.
-Place your final answer between <answer> and </answer>.
-"""
-
-            try:
-                inputs, outputs = map(
-                    list, zip(*list(filter(remove_long, zip(inputs, outputs))))
-                )
-                data = {
-                    "data_source": data_source,
-                    "prompt": [
-                         {
-                            "role": "system",
-                            "content": sys_prompt,
-                        },
-                        {
-                            "role": "user",
-                            "content": question,
-                        }
-                    ],
-                    "ability": "math",
-                    "reward_model": {
-                        "style": "rule",
-                        "ground_truth": {"inputs": inputs, "outputs": outputs},
-
-                    },
-                    "extra_info": {
-                        "split": split,
-                        "index": idx,
-                        "question": question_raw,
-                    },
-                }
-                return data
-            except:
-                #print(idx)
-                data = {
-                    "data_source": data_source,
-                    "prompt": [
-                        {
-                            "role": "system",
-                            "content": sys_prompt,
-                        },
-                        {
-                            "role": "user",
-                            "content": question,
-                        }
-                    ],
-                    "ability": "math",
-                    "reward_model": {
-                        "style": "rule",
-                        "ground_truth": {"inputs": [], "outputs": []},
-                    },
-                    "extra_info": {
-                        "split": split,
-                        "index": idx,
-                        "question": question_raw,
-                    },
-                }
-        return process_fn
-
-    train_dataset = train_dataset.map(function=make_map_fn("train"), with_indices=True)
-    test_dataset = test_dataset.map(function=make_map_fn("test"), with_indices=True)
-
-    local_dir = args.local_dir
-    hdfs_dir = args.hdfs_dir
-
-    train_dataset.to_parquet(os.path.join(local_dir, "TACO_train.parquet"))
-    test_dataset.to_parquet(os.path.join(local_dir, "TACO_test.parquet"))
-
-    if hdfs_dir is not None:
-        makedirs(hdfs_dir)
-
-        copy(src=local_dir, dst=hdfs_dir)
+            return {
+                "style": "rule",
+                "ground_truth": {"inputs": input_output_pair[0], "outputs": input_output_pair[1]},
+            }
+        reward_model = list(map(format_reward_model, zip(prompts, ground_truth)))
+        df = pd.DataFrame({
+            "data_source" : data_source,
+            "prompt": prompt,
+            "ability": ability,
+            "reward_model": reward_model
+        })
+        print("Writing to parquet...")
+        df = df.head(5)
+        df.to_parquet(f"TACO_{split}_processed.parquet")
+        print("done")
+        print(df.head())
+    make_parquet("train")
+    make_parquet("test")
--- scripts/Reasoner/reward_function.py
+++ scripts/Reasoner/reward_function.py
@@ -9,12 +9,15 @@ import numpy as n
 
 
 def compute_score(data_source, solution_str, ground_truth, extra_info=None):
-    loaded_ground = json.loads(ground_truth)
-    case_input = loaded_ground["inputs"]
-    case_output = loaded_ground["outputs"]
+    #loaded_ground = json.loads(ground_truth)
+    case_input = ground_truth["inputs"]
+    case_output = ground_truth["outputs"]
     return eval_solution(solution_str, case_input, case_output)
 
 def eval_solution(solution_str, case_input, case_output):
     reward = 0
-    solution_str = re.findall("<answer>(.*?)</answer>", solution_str, re.DOTALL)
-    if solution_str =
+    solution_str = re.findall(r"<think>.*</think>(.*)", solution_str, re.DOTALL)
+    if solution_str == case_output:
+        reward = 1
+    return reward
+    
