8c3dd3ca3ec147c590a19e68114c78de9361dde9
5plax <aaronluoyj@gmail.com>
2025-07-02 14:02:53 -0700

    added LoRA to SFTTrainer
    Signed-off-by: 5plax <aaronluoyj@gmail.com>

--- scripts/SFT/train.py
+++ scripts/SFT/train.py
@@ -1,6 +1,6 @@
 import datasets
 from trl import SFTConfig, SFTTrainer
-
+from peft import LoraConfig
 
 def main():
     train_dataset = datasets.load_from_disk("train.hf")
@@ -10,11 +10,21 @@ def main():
         max_length=2048,
         output_dir="/tmp",
     )
+    
+    peft_config = LoraConfig(
+   	r=16,
+   	lora_alpha=32,
+    	lora_dropout=0.05,
+    	task_type="CAUSAL_LM",
+    )
+
     trainer = SFTTrainer(
         "facebook/opt-350m",
         train_dataset=train_dataset,
         args=training_args,
+	peft_config=pef_config
     )
+
     trainer.train()
     return
 
