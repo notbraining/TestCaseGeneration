ffb20dad0a72d7f16d0d9726d9005c5d2c5ee525
Brian Zhao <brianzhao@Brians-Air.attlocal.net>
2025-07-03 10:49:12 -0700

    added install.sh

---
+++ scripts/Reasoner/test.hf/install.sh
@@ -0,0 +1,12 @@
+pip install transformers
+pip install peft
+pip install bitsandbytes
+pip install datasets
+pip install trl
+pip install tf-keras
+pip install --upgrade pandas
+pip install numpy==1.26.4
+pip install gpustat
+pip install dotenv
+pip install multiprocess
+pip install openai
