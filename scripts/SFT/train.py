59ae32f89bd4bede4ef954c0d725f5d93cdf13e9
Charlie Huang <charliehuang09@gmail.com>
2025-07-03 10:47:42 -0700

    fix bugs
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- scripts/SFT/train.py
+++ scripts/SFT/train.py
@@ -1,28 +1,32 @@
 import datasets
+from datasets import Dataset
+from typing import cast
 from trl import SFTConfig, SFTTrainer
 from peft import LoraConfig
 
+
 def main():
-    train_dataset = datasets.load_from_disk("train.hf")
-    # test_dataset = datasets.load_from_disk("test.hf")
+    train_dataset: Dataset = cast(Dataset, datasets.load_from_disk("train.hf"))
+    test_dataset: Dataset = cast(Dataset, datasets.load_from_disk("test.hf"))
 
     training_args = SFTConfig(
         max_length=2048,
         output_dir="/tmp",
     )
-    
+
     peft_config = LoraConfig(
-   	r=16,
-   	lora_alpha=32,
-    	lora_dropout=0.05,
-    	task_type="CAUSAL_LM",
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        task_type="CAUSAL_LM",
     )
 
     trainer = SFTTrainer(
         "facebook/opt-350m",
         train_dataset=train_dataset,
+        eval_dataset=test_dataset,
         args=training_args,
-	peft_config=pef_config
+        peft_config=peft_config,
     )
 
     trainer.train()
